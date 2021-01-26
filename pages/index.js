2d24b78dd5d3053ccbcf53bb3a19a8774757526a
Dhiego Cleones <cleones06@gmail.com>
2021-01-26 13:55:15 -0400

    .

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,46 @@
 import styled from 'styled-components'
+import db from '../db.json'
+import Widget from '../src/components/Widget';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizBackground from '../src/components/QuizBackground';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
 
-export default function Home() {
-  return <Title>My page</Title>
+const QuizContent = styled.div`
+  width:100%;
+  max-width: 350px;
+  padding-top: 10%;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
+const Home = () =>  {
+  return (
+    <QuizBackground backgroundImage={db.bg}> 
+      <QuizContent>
+        <Widget>
+          <Widget.Header>
+            <h1>The legend of zelda</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p> Lorem ipsum dolor sit amet.....</p>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Header>
+            <h1>The legend of zelda</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p> Lorem ipsum dolor sit amet.....</p>
+          </Widget.Content>
+        </Widget>
+        <Footer/>
+      </QuizContent>
+      <GitHubCorner projectUrl="https://github.com/cleones" />
+    </QuizBackground>
+  )
 }
+
+export default Home
