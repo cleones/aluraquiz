373a1e03544ca2c7d4f69cf87c2625f6e17f3eaa
Dhiego Cleones <cleones06@gmail.com>
2021-01-31 17:15:34 -0400

    fullProject

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
-import styled from 'styled-components'
-import db from '../db.json'
+import React, { useCallback, useState } from 'react';
+import { useRouter } from 'next/router';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { motion } from 'framer-motion';
+import db from '../db.json';
 import Widget from '../src/components/Widget';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
 import QuizBackground from '../src/components/QuizBackground';
-
+import QuizLogo from '../src/components/QuizLogo';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
+import Link from '../src/components/Link';
 
 const QuizContent = styled.div`
   width:100%;
@@ -16,31 +23,95 @@ const QuizContent = styled.div`
     padding: 15px;
   }
 `;
-const Home = () =>  {
+const Home = () => {
+  const [name, setName] = useState('');
+  const router = useRouter();
+
+  const handleSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      router.push(`/quiz?name=${name}`);
+    },
+    [],
+  );
   return (
-    <QuizBackground backgroundImage={db.bg}> 
+    <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>AluraQuiz - Modelo Base</title>
+      </Head>
       <QuizContent>
-        <Widget>
+        <QuizLogo />
+        <Widget
+          as={motion.section}
+          transition={{ delay: 0, durante: 0.5 }}
+          variants={{
+            show: { opacity: 1, y: '0' },
+            hidden: { opacity: 0, y: '100%' },
+          }}
+          initial="hidden"
+          animate="show"
+        >
           <Widget.Header>
             <h1>The legend of zelda</h1>
           </Widget.Header>
           <Widget.Content>
-            <p> Lorem ipsum dolor sit amet.....</p>
+            <form onSubmit={handleSubmit}>
+              <Input name="nomeDoJogado" value={name} onChange={(e) => setName(e.target.value)} placeholder="Diz ai seu nome" />
+              <Button type="submit" disabled={!name}>
+                {`Jogar ${name}`}
+              </Button>
+            </form>
           </Widget.Content>
         </Widget>
-        <Widget>
-          <Widget.Header>
-            <h1>The legend of zelda</h1>
-          </Widget.Header>
+        <Widget
+          as={motion.section}
+          transition={{ delay: 0.5, durante: 0.5 }}
+          variants={{
+            show: { opacity: 1 },
+            hidden: { opacity: 0 },
+          }}
+          initial="hidden"
+          animate="show"
+        >
           <Widget.Content>
-            <p> Lorem ipsum dolor sit amet.....</p>
+            <h1>Quiz da Galera</h1>
+            <ul>
+              {db.external.map((linkExterno) => {
+                const [projectName, githubUser] = linkExterno
+                  .replace(/\//g, '')
+                  .replace('https:', '')
+                  .replace('.vercel.app', '')
+                  .split('.');
+
+                return (
+                  <li key={linkExterno}>
+                    <Widget.Topic
+                      as={Link}
+                      href={`/quiz/${projectName}___${githubUser}`}
+                    >
+                      {`${githubUser}/${projectName}`}
+                    </Widget.Topic>
+                  </li>
+                );
+              })}
+            </ul>
+
           </Widget.Content>
         </Widget>
-        <Footer/>
+        <Footer
+          as={motion.section}
+          transition={{ delay: 0, durante: 0.5 }}
+          variants={{
+            show: { opacity: 1, y: '0' },
+            hidden: { opacity: 0, y: '100%' },
+          }}
+          initial="hidden"
+          animate="show"
+        />
       </QuizContent>
       <GitHubCorner projectUrl="https://github.com/cleones" />
     </QuizBackground>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
